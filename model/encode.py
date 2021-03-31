ee7694fb1a3e642522d9f43f24e864b751337b4a
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-31 17:43:35 +0530

    various testing on cleanup code

--- model/encode.py
+++ model/encode.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, OneHotEncoder
 
+
 def encode(X):
     cont = X.select_dtypes(exclude='object')
     cat = X.select_dtypes(include='object')
 
-    ohe = OneHotEncoder(drop='first')
+    # ohe = OneHotEncoder(drop='first')
+    ohe = OneHotEncoder()
     train = ohe.fit_transform(cat).toarray()
-    train_df= pd.DataFrame(train, columns=ohe.get_feature_names(cat.columns))
+    train_df = pd.DataFrame(train, columns=ohe.get_feature_names(cat.columns))
+    # print(train_df["Gender_M"])
     train_df.head(2)
 
     train_df.reset_index(inplace=True, drop='first')
