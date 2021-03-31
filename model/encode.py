476a05584da94e82a1e3df08b5a1c24991375f25
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-03-31 16:30:04 +0530

    added data cleaning code to notebook

---
+++ model/encode.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, OneHotEncoder
+
+def encode(X):
+    cont = X.select_dtypes(exclude='object')
+    cat = X.select_dtypes(include='object')
+
+    ohe = OneHotEncoder(drop='first')
+    train = ohe.fit_transform(cat).toarray()
+    train_df= pd.DataFrame(train, columns=ohe.get_feature_names(cat.columns))
+    train_df.head(2)
+
+    train_df.reset_index(inplace=True, drop='first')
+    cont.reset_index(inplace=True, drop='first')
+
+    return train_df.join(cont)
