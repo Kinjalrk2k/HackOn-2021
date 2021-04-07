44e0969f3bcbce1c762302f01315d7aed641ba07
Somsubhra Das <das.somsubhra1@gmail.com>
2021-04-07 16:32:39 +0530

    cors added

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 
 from model.run import run_model, build_input
 
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Input(BaseModel):
